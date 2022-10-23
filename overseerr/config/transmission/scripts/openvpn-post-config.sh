58b29fea1968f2b649ddf5bab55c10c78ded0056
root <root@docker.ferg.ferguson.pw>
2022-10-23 21:49:20 +0000

    Update script and docker container to work with mam

--- overseerr/config/transmission/scripts/openvpn-post-config.sh
+++ overseerr/config/transmission/scripts/openvpn-post-config.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl -c /data/transmission-home/mam.cookies -b /data/transmission-home/mam.cookies https://t.myanonamouse.net/json/dynamicSeedbox.php
+#https://www.myanonamouse.net/api/endpoint.php/3/json/dynamicSeedbox.php
+#sleep 15
+curl -c /var/transmission-home/mam.cookies -b /var/transmission-home/mam.cookies https://t.myanonamouse.net/json/dynamicSeedbox.php
