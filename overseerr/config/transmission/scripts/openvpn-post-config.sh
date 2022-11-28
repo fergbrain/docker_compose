57ae3b085d6db57fc91c0f91e537e41d8b900e12
root <root@docker.ferg.ferguson.pw>
2022-11-28 19:54:53 +0000

    Update location to preferred

--- overseerr/config/transmission/scripts/openvpn-post-config.sh
+++ overseerr/config/transmission/scripts/openvpn-post-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #https://www.myanonamouse.net/api/endpoint.php/3/json/dynamicSeedbox.php
 #sleep 15
-curl -c /var/transmission-home/mam.cookies -b /var/transmission-home/mam.cookies https://t.myanonamouse.net/json/dynamicSeedbox.php
+curl -c /config/transmission-home/mam.cookies -b /config/transmission-home/mam.cookies https://t.myanonamouse.net/json/dynamicSeedbox.php
