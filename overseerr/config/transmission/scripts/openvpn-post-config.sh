5b308304cc49a99806075da697ce3278473c952e
root <root@docker.ferg.ferguson.pw>
2023-04-11 19:24:03 +0000

    Clean up transmission file and setup for using 10.200.0.0/24 for VPN access

--- overseerr/config/transmission/scripts/openvpn-post-config.sh
+++ overseerr/config/transmission/scripts/openvpn-post-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #https://www.myanonamouse.net/api/endpoint.php/3/json/dynamicSeedbox.php
 #sleep 15
+echo "Setting cookie"
 curl -c /config/transmission-home/mam.cookies -b /config/transmission-home/mam.cookies https://t.myanonamouse.net/json/dynamicSeedbox.php
